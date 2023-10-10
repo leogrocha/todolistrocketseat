cff24c1fc7353b610a3646ed3672a7313465368b
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-10-10 00:25:32 -0300

    Finalizando primeira aula

---
+++ src/main/java/com/estudosrocketseat/todolist/user/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.estudosrocketseat.todolist.user.controller;
+
+import com.estudosrocketseat.todolist.user.domain.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody User user) {
+        System.out.println(user.getName());
+    }
+
+}
